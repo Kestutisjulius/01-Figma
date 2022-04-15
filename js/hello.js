3aee5cbfc79be51a9e4910c9ac24ea3cc457f652
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-04-15 22:27:02 +0300

    Friday chalenge commit 'good morning' + Time

--- js/hello.js
+++ js/hello.js
@@ -1,3 +1,29 @@
 function display() {  
     alert("Hello Class BIT-35!");  
     }  
+
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('time').innerHTML =
+        h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+
+    var timeDisplay = document.getElementById("date");
+
+    function refreshTime() {
+    var dateString = new Date().toLocaleString("en-US", {timeZone: "Europe/Vilnius"});
+    var formattedString = dateString.replace(", ", " - ");
+    timeDisplay.innerHTML = formattedString;
+    }
+
+setInterval(refreshTime, 1000);
